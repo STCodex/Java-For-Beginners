0f436075d13ce27c847e9a8b99857fa349d9ebc5
Abdul Shakoor Abdul Azeem Mousa <shoukreytom01@gmail.com>
2021-09-04 13:06:36 +0200

    code: source codes for variables lesson

---
+++ Variables1.java
@@ -0,0 +1,18 @@
+public class Variables1{
+    
+    public static void main(String[] args) {
+        int x;
+        float y;
+        char ch;
+        boolean bool;
+        x = 8;
+        y = 88.89f;
+        ch = 'A';
+        bool = true;
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(ch);
+        System.out.println(bool);
+    }
+
+}
---
+++ Variables2.java
@@ -0,0 +1,19 @@
+public class Variables2 {
+	public static void main(String[] args) {
+		byte b;
+		double d;
+		long lo;
+		short s;
+		String str;
+		b = 12;
+		d = 983892.983839d;
+		lo = 3928398;
+		s = 203;
+		str = "Hello";
+		System.out.println(b);
+		System.out.println(d);
+		System.out.println(lo);
+		System.out.println(s);
+		System.out.println(str);
+	}
+}
